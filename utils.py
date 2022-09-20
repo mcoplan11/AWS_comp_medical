5f8885dd65e9886ad40f85054c1759050f2ecbee
mitch <mitchell_coplan@premierinc.com>
2022-09-19 21:57:18 -0500

    updates

--- utils.py
+++ utils.py
@@ -1,12 +1,5 @@
 import re
 
-regex_keywords = [
-    r'LVEF less than (\d||d\d)% is considered NYHA',
-    r'LEFT VENTRICULAR EJECTION FRACTION in the range of 50% to 70% is normal',
-]
-regex_keywords = '|'.join(f'({p})' for p in regex_keywords)
-
-
 list_of_lvef_entities = [
     'LEFT VENTRICULAR EJECTION FRACTION'
     , 'LEFT VENTRICULAR EJECTION FRATION'
@@ -20,7 +13,15 @@ list_of_lvef_entities = [
     , 'EJECTION FRACTION'
 ]
 
+#filter out common keywords/phrases that lead to false positives
+regex_keywords = [
+    r'LVEF less than (\d||d\d)% is considered NYHA',
+    r'LEFT VENTRICULAR EJECTION FRACTION in the range of 50% to 70% is normal',
+]
+regex_keywords = '|'.join(f'({p})' for p in regex_keywords)
+
 def pre_process_text(text: str = ''):
+    #TODO: add a spellcorrect here
     if re.search(regex_keywords, text, flags=re.I):
         print(f'!!! Filtering out this doc: {text}')
         return ''
